a326b53e3b5504c60ea6d104070ae4c9de38b736
Samuel <samuel.bandeira.oliveira@gmail.com>
2023-07-18 23:00:45 -0300

    Started Photo component implementation

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Photo :user="user" :imageUrl="imageUrl" />
+  </div>
+</template>
+<script>
+import Photo from "./components/Photo.vue";
+
+export default {
+  components: { Photo },
+  data() {
+    return {
+      user: {
+        id: 1,
+        name: "Samuel_Oliveira",
+      },
+      imageUrl: "https://shorturl.at/iAT04",
+    };
+  },
+};
+</script>
---
+++ src/components/Photo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="photo">
+    <div class="header">
+      <div class="user-picture">
+        <img :src="imageUrl" />
+      </div>
+      <div>
+        <p>{{ user.name }}</p>
+      </div>
+    </div>
+    <div class="content">
+      <img :src="imageUrl" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Photo",
+  props: {
+    user: {
+      type: Object,
+      default: {},
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style src="./style.css"></style>
